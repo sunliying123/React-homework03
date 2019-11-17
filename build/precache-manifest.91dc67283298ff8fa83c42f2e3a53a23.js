7069c76bb337d518c6aad4e23308189a74d208da
孙丽颖 <33644965+sunliying123@users.noreply.github.com>
2019-11-17 14:06:32 +0800

    Add files via upload

---
+++ build/precache-manifest.91dc67283298ff8fa83c42f2e3a53a23.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce465460921fb209df6221fd9bbb4296",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76acae83c4fcc9c07b4a",
+    "url": "/static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "76acae83c4fcc9c07b4a",
+    "url": "/static/js/2.8856006f.chunk.js"
+  },
+  {
+    "revision": "6a802cdb6d4be41291e8",
+    "url": "/static/js/main.248801a5.chunk.js"
+  },
+  {
+    "revision": "14c23a88433f623ab3ae",
+    "url": "/static/js/runtime-main.e2c7d5a8.js"
+  },
+  {
+    "revision": "47dbe24f2ef833d16638af2043fcbe6f",
+    "url": "/static/media/bed1.47dbe24f.jpg"
+  },
+  {
+    "revision": "0ca2de90f202290b81838f1c557a61a1",
+    "url": "/static/media/bed2.0ca2de90.jpg"
+  },
+  {
+    "revision": "de01716691637f2d376715c3f4c767de",
+    "url": "/static/media/chuang.de017166.jpg"
+  },
+  {
+    "revision": "3459114c2da33f40e6aa7d390d21e372",
+    "url": "/static/media/clock.3459114c.jpg"
+  },
+  {
+    "revision": "93a894154022f4457f0167b0b3d4025b",
+    "url": "/static/media/cup.93a89415.jpg"
+  },
+  {
+    "revision": "0ed0ba7edbc98b88ab93605b8e72a5dd",
+    "url": "/static/media/feel1.0ed0ba7e.jpg"
+  },
+  {
+    "revision": "9ac8c9a86bb04be35ccccced11530696",
+    "url": "/static/media/feel2.9ac8c9a8.jpg"
+  },
+  {
+    "revision": "718f2e25e7240b482be42721033810fc",
+    "url": "/static/media/feel3.718f2e25.jpg"
+  },
+  {
+    "revision": "78e6af9047e0492fe909dc1703c06cfb",
+    "url": "/static/media/feel4.78e6af90.jpg"
+  },
+  {
+    "revision": "196d975a3f6791bb6b3de5078c8dfc5f",
+    "url": "/static/media/gui.196d975a.jpg"
+  },
+  {
+    "revision": "007ab9f5414882a4482af38099a1227a",
+    "url": "/static/media/home3.007ab9f5.jpg"
+  },
+  {
+    "revision": "c51577831f62990d0db8ec5087e237f6",
+    "url": "/static/media/home4.c5157783.jpg"
+  },
+  {
+    "revision": "b452c12c5b4ade30881687ccc99065e0",
+    "url": "/static/media/home5.b452c12c.jpg"
+  },
+  {
+    "revision": "0c87c43e09367bb0e33448d60ed26c98",
+    "url": "/static/media/home6.0c87c43e.jpg"
+  },
+  {
+    "revision": "4e7622e9fe570f51510bb73a486b45e3",
+    "url": "/static/media/huwai.4e7622e9.jpg"
+  },
+  {
+    "revision": "3c5805db300220417de9fe6cb1aebc8c",
+    "url": "/static/media/ji.3c5805db.jpg"
+  },
+  {
+    "revision": "8335ff93064afd170ed6a33d82a61e45",
+    "url": "/static/media/jiaju.8335ff93.jpg"
+  },
+  {
+    "revision": "f83bd4f29f3982bdf8e2f7a5935570f7",
+    "url": "/static/media/quanbu.f83bd4f2.jpg"
+  },
+  {
+    "revision": "840d54c831ff149bbc87ead3f462b3ca",
+    "url": "/static/media/shafa.840d54c8.jpg"
+  },
+  {
+    "revision": "69a4c766069c2b14112f8d94d4ae73d5",
+    "url": "/static/media/shop1.69a4c766.jpg"
+  },
+  {
+    "revision": "7cce341b911871e7ed94433b65539c1c",
+    "url": "/static/media/shop2.7cce341b.jpg"
+  },
+  {
+    "revision": "a7865af07c38e0322cb51d2ea7f6f0a3",
+    "url": "/static/media/shop3.a7865af0.jpg"
+  },
+  {
+    "revision": "89e2024a83be41758e7004fc5f2b3af9",
+    "url": "/static/media/shujia.89e2024a.jpg"
+  },
+  {
+    "revision": "e168b506663b7c0950a74507e57deb07",
+    "url": "/static/media/yi.e168b506.jpg"
+  },
+  {
+    "revision": "d1ff8e57b4ae3a6959dcf82434421f83",
+    "url": "/static/media/zhuo.d1ff8e57.jpg"
+  }
+]);
